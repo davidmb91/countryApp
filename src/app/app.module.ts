406cc28d48c91e71e59af29a3d51c172841e07c9
David Moreno Buj <davidmb91@gmail.com>
2023-07-31 15:11:14 +0200

    Router module create

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     SharedModule
   ],
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { RouterModule } from '@angular/router';
+
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AboutPageComponent,
+    HomePageComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+  ],
+  exports:[
+    AboutPageComponent,
+    HomePageComponent
   ]
 })
 export class SharedModule { }
