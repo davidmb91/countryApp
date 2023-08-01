d4b028e85c60dd8d9244c05288a9153f976c4aa5
David Moreno Buj <davidmb91@gmail.com>
2023-08-01 15:59:52 +0200

    Country table component

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -6,12 +6,12 @@ import { Country } from '../interfaces/country';
 @Injectable({providedIn: 'root'})
 export class CountriesService {
 
-  private apiUrl: string = 'https://restcountries.com/v3.1';
+  private apiUrl: string = 'https://restcountries.com/v3.1/';
 
   constructor(private http: HttpClient) { }
 
   searchCapital(term: string): Observable<Country[]> {
-    return this.http.get<Country[]>(`${this.apiUrl}/name/${term}`);
+    return this.http.get<Country[]>(`${this.apiUrl}/capital/${term}`);
   }
 
 }
