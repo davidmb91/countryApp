471258beea655d325a934d0b240d816e5e506c4c
David Moreno Buj <davidmb91@gmail.com>
2023-08-01 16:15:02 +0200

    Catch error for service

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({providedIn: 'root'})
@@ -11,7 +11,11 @@ export class CountriesService {
   constructor(private http: HttpClient) { }
 
   searchCapital(term: string): Observable<Country[]> {
-    return this.http.get<Country[]>(`${this.apiUrl}/capital/${term}`);
+    return this.http.get<Country[]>(`${this.apiUrl}/capital/${term}`).pipe(
+      catchError( error => {
+        return of([]);  // retornamos observable vacío
+      } )
+    );
   }
 
 }
