1e5e290f43c7e14b996a92aaa4d0edec16afd763
David Moreno Buj <davidmb91@gmail.com>
2023-08-01 14:54:31 +0200

    SearchBox Component init

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -5,7 +5,9 @@ import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.
 import { ByCountryPageComponent } from './pages/by-country-page/by-country-page.component';
 import { ByRegionPageComponent } from './pages/by-region-page/by-region-page.component';
 import { CountryPageComponent } from './pages/country-page/country-page.component';
+
 import { CountriesRoutingModule } from './countries-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -18,7 +20,8 @@ import { CountriesRoutingModule } from './countries-routing.module';
   ],
   imports: [
     CommonModule,
-    CountriesRoutingModule
+    CountriesRoutingModule,
+    SharedModule
   ]
 })
 export class CountriesModule { }
